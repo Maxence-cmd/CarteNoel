3c62dd473916e703a9bd627017241de8cc840ef7
SLAB48 <SLAB48@KM101-13-M>
2025-11-14 09:55:10 +0100

    (t-èiu((-'"é&

---
+++ CarteNoel/Window1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CarteNoel
+{
+    /// <summary>
+    /// Logique d'interaction pour Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            
+            changerimage();
+        }
+        private void BackgroundVideo_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            BackgroundVideo.Position = TimeSpan.Zero;
+            BackgroundVideo.Play();
+        }
+        private void tbNom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        public void changerimage()
+        { 
+                
+                imgboite.Source = new BitmapImage(new Uri("pack://application:,,,/Image/BoiteLettresPleine.png"));
+        }
+    }
+}
